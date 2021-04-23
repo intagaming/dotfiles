75d718acc1cbb37a149c6b5925260234fb6f9593
An Xuan Hoang <xuanan2001@gmail.com>
2021-04-23 13:17:18 +0700

    config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,9 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -rq bar &
-#polybar music &
-#polybar tray &
-#polybar padding &
+polybar -rq eDP1 &
+polybar -rq HDMI1 &
 
 echo "Polybar launched..."
