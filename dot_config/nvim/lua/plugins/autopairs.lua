7d7ad05e07ae16c8f91f77f3b7529d6759098f20
An Hoang <xuanan2001@pm.me>
2023-06-20 10:55:53 +0700

    update nvim config

--- dot_config/nvim/lua/plugins/autopairs.lua
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -1,24 +1,10 @@
 return {
     'windwp/nvim-autopairs',
+    event = "InsertEnter",
     config = function()
         require("nvim-autopairs").setup {}
 
-        -- Who designs an API that needs to know the defaults to add a thing?
-        local defaultFiletypes = {
-            'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx',
-            'rescript',
-            'xml',
-            'php',
-            'markdown',
-            'glimmer', 'handlebars', 'hbs'
-        }
-
-        local filetypes = defaultFiletypes
-        table.insert(filetypes, 'astro')
-
-        require('nvim-ts-autotag').setup {
-            filetypes = filetypes
-        }
+        require('nvim-ts-autotag').setup {}
 
         -- Insert `(` after autocomplete function and method item
         local cmp_autopairs = require('nvim-autopairs.completion.cmp')
