fd8ddbe6f5ece33352eb52ff659b610d75ca5aff
An Hoang <xuanan2001@pm.me>
2023-07-24 00:12:57 +0700

    treesitter prefer_git

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
         ts_update()
     end,
     config = function()
+        require("nvim-treesitter.install").prefer_git = true
         require 'nvim-treesitter.configs'.setup {
             -- A list of parser names, or "all" (the four listed parsers should always be installed)
             ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust", "vim" },
