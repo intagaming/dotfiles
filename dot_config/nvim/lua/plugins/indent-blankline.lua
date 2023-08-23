38071a9695764722a608af931cadc4427293b385
An Hoang <xuanan2001@pm.me>
2023-08-23 17:33:19 +0700

    disable space indicators

--- dot_config/nvim/lua/plugins/indent-blankline.lua
+++ dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -1,8 +1,8 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-        vim.opt.list = true
-        vim.opt.listchars:append "space:⋅"
+        -- vim.opt.list = true
+        -- vim.opt.listchars:append "space:⋅"
 
         require("indent_blankline").setup {
             show_current_context = true,
