631ac1bf444670cccdc47cdb662ecbee0a260b25
An Hoang <xuanan2001@pm.me>
2024-03-28 17:09:35 +0700

    remove ssh config; vscode remap

--- dot_config/nvim/lua/an7/remap.lua
+++ dot_config/nvim/lua/an7/remap.lua
@@ -15,6 +15,9 @@ if vim.g.vscode then
     vim.keymap.set("n", "[d", function()
         vscode.action("editor.action.marker.prev")
     end)
+    vim.keymap.set("i", "<C-h>", function()
+        vscode.action("editor.action.triggerParameterHints")
+    end)
 else
     vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
 
